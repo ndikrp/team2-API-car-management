ac02c3ad1eade3874318a23debc0a8b950cfc156
Hafieddz <hasmy41@gmail.com>
2024-04-19 20:22:50 +0800

    fix : delete unused route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 
 const Car = require("./carRouter");
-const Admin = require("./adminRouter");
 const Auth = require("./authRouter");
 const User = require("./userRouter");
 const Details = require("./detailsRouter");
@@ -11,6 +10,5 @@ router.use("/api/v1/auth", Auth);
 router.use("/api/v1/users", User);
 router.use("/api/v1/details", Details);
 
-router.use("/", Admin);
 
 module.exports = router;
