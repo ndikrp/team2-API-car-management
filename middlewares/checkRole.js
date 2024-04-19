4c8838b9ca55761e98766bab296892d68488d89c
Hafieddz <hasmy41@gmail.com>
2024-04-19 21:35:04 +0800

    fix : checkrole middleware

--- middlewares/checkRole.js
+++ middlewares/checkRole.js
@@ -3,8 +3,13 @@ const ApiError = require("../utils/apiError");
 const checkRole = (roles) => {
   return async (req, res, next) => {
     try {
+      let allowedRoles;
       if (!roles.includes(req.user.role)) {
-        const allowedRoles = roles.join(" or ");
+        if (roles.length > 1) {
+          allowedRoles = roles.join(" atau ");
+        } else {
+          allowedRoles = roles.join(" ");
+        }
         next(
           new ApiError(
             `Akses anda tidak diterima. Role yang memiliki akses : ${allowedRoles}`,
