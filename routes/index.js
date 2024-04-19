29740b4bbab4b759f7362d727a2c88194e630c00
Hafieddz <hasmy41@gmail.com>
2024-04-19 20:27:03 +0800

    feat : add rental route in main file

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const Car = require("./carRouter");
 const Auth = require("./authRouter");
 const User = require("./userRouter");
 const Details = require("./detailsRouter");
+const Rental = require('./rentalRouter');
 
 router.use("/api/v1/cars", Car);
 router.use("/api/v1/auth", Auth);
 router.use("/api/v1/users", User);
 router.use("/api/v1/details", Details);
+router.use("/api/v1/rentals", Rental);
 
 
 module.exports = router;
