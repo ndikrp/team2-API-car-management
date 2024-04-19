2dc5432fe0d9e1baca330d56ffe63e05094b3a7b
Hafieddz <hasmy41@gmail.com>
2024-04-19 22:53:01 +0800

    feat : update route

--- routes/detailsRouter.js
+++ routes/detailsRouter.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 
 const Details = require("../controllers/detailsController");
-const authenticate = require("../middlewares/authenticate");
 const upload = require("../middlewares/uploader");
+const autentikasi = require("../middlewares/authenticate");
+const checkRole = require("../middlewares/checkRole");
 
-router.get("/", authenticate, Details.getDetails);
-router.post("/", authenticate, upload.array("images"), Details.createDetails);
+router.get("/", autentikasi, Details.getDetails);
+router.post("/", autentikasi, checkRole(["Admin", "Manager"]), upload.array("images"), Details.createDetails);
+router.patch("/:id", autentikasi, checkRole(["Admin", "Manager"]), upload.array("images"), Details.updateDetails);
+
+router.get("/:id", autentikasi, Details.findDetailsById);
+router.delete("/:id", autentikasi, Details.deleteDetails);
 
 module.exports = router;
