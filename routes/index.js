d3fcc2b4ab7dcdd63601fb2cfdf81f70c6b4513e
Hafieddz <hasmy41@gmail.com>
2024-04-19 20:19:24 +0800

    fix : delete unused route

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,11 @@ const Car = require("./carRouter");
 const Admin = require("./adminRouter");
 const Auth = require("./authRouter");
 const User = require("./userRouter");
-const Shop = require("./shopRouter");
 const Details = require("./detailsRouter");
 
 router.use("/api/v1/cars", Car);
 router.use("/api/v1/auth", Auth);
 router.use("/api/v1/users", User);
-router.use("/api/v1/shops", Shop);
 router.use("/api/v1/details", Details);
 
 router.use("/", Admin);
