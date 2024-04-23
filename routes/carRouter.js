b986d77440008c9afa8f17f6de6e343cea8083eb
ndikrp <andhikarizky36@gmail.com>
2024-04-23 22:33:45 +0700

    implement middleware to car routes, naming consistency

--- routes/carRouter.js
+++ routes/carRouter.js
@@ -7,16 +7,12 @@ const autentikasi = require("../middlewares/authenticate");
 const checkRole = require("../middlewares/checkRole");
 const checkOwnership = require("../middlewares/checkOwnership");
 
-router.post(
-  "/",
-  autentikasi,
-  checkRole(["Admin", "Manager"]),
-  upload.array("images"),
-  Car.createProduct
-);
-router.get("/", autentikasi, Car.findProducts);
-router.get("/:id", autentikasi, Car.findProductById);
-router.patch("/:id", Car.UpdateProduct);
-router.delete("/:id", Car.deleteProduct);
+router.post("/", autentikasi, checkRole(["Admin", "Manager"]),
+upload.array("images"), Car.createCar);
+
+router.get("/", autentikasi, Car.findCars);
+router.get("/:id", autentikasi, Car.findCarById);
+router.patch("/:id", autentikasi, checkRole(["Admin", "Manager"]), Car.UpdateCar);
+router.delete("/:id", autentikasi, checkRole(["Admin"]), Car.deleteCar);
 
 module.exports = router;
